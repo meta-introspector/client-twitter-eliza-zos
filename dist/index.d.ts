4a86a028835a10324774a8e14fb513ef3d8d01f9
mike dupont <mike.dupont@introspector.local>
2025-02-22 13:45:30 -0500

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as _elizaos_core from '@elizaos/core';
-
-declare const twitterPlugin: {
-    name: string;
-    description: string;
-    clients: _elizaos_core.Client[];
-};
-
-export { twitterPlugin as default };
